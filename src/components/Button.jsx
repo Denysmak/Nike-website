f2091a66da32075600955e424d865a04580730f3
Denys Makene <denysmakene13@gmail.com>
2024-08-02 21:51:16 -0300

    termina seção SpecialOffer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,14 @@
-const Button = ({label, iconUrl}) => {
+const Button = ({label, iconUrl, backgroundColor, textColor, borderColor}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white max-sm:w-full">
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+    
+    ${
+      backgroundColor
+      ? `${backgroundColor} $
+      {textColor} ${borderColor}`
+      : "bg-coral-red text-white border-coral-red"
+    } rounded-full max-sm:w-full`}
+    >
         {label}
 
        {iconUrl &&  <img src={iconUrl} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5"/>}
