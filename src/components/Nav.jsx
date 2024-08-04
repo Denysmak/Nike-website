872c6af547593c90b23255bb290c1403a091cce3
Denys Makene <denysmakene13@gmail.com>
2024-08-04 16:36:19 -0300

    adiciona funcionalidade ao botão hamburguer

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,30 +1,43 @@
+import React, { useState } from 'react';
 import { headerLogo } from '../assets/images';
 import { hamburger } from '../assets/icons';
-import {navLinks} from '../constants/index'
+import { navLinks } from '../constants/index';
+
 const Nav = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
-        <nav className='flex justify-between items-center max-container'>
-            <a href="/">
-                <img src={headerLogo} alt="Logo" width={130} height={29}/>
-            </a>
-            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden '>
-
-            {navLinks.map((item) =>  (
-                <li key={item.label}>
-                    <a href={item.href} className='font-monsterrat leading-normal text-lg text-slate-gray'>
-                        {item.label}
-                    </a>
-                </li>
-            ))}
-
-            </ul>
-            <div className='hidden  max-lg:block'>
-                <img src={hamburger} alt="Hamburguer" width={24} height={25} />
-            </div>
-        </nav>
+      <nav className='flex justify-between items-center max-container border relative'>
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+        <ul className={`flex-1 flex justify-center items-center gap-16 ${isMenuVisible ? 'block' : 'hidden'} lg:flex max-lg:absolute max-lg:left-[80%] max-lg:top-[70px] max-lg:flex-col max-lg:bg-slate-50 max-lg:border max-lg:border-gray-400 max-lg:w-[160px] rounded-2xl p-5 max-md:w-[140px] max-md:left-[70%] max-sm:left-1/2 max-sm:-translate-x-1/2 max-sm:w-[80%]`}>
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a href={item.href} className='font-monsterrat leading-normal text-lg text-slate-gray' onClick={toggleMenu} >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className='lg:hidden'>
+          <img
+            src={hamburger}
+            alt="Hamburguer"
+            width={24}
+            height={25}
+            onClick={toggleMenu}
+            className='cursor-pointer'
+          />
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
